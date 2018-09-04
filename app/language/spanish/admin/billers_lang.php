5fefeb91663763f784cb96c2ba3d4edc977d6495
marcelahernandez27 <marcela.hernandez27@gmail.com>
2018-09-04 11:26:49 -0600

    Primer Checkin
    test para la primera checkin

---
+++ app/language/spanish/admin/billers_lang.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * última edición: 22/03/2015
+ * Ricardo Ramírez R.
+ * Adan Rivera - 12/08/2016
+ */
+
+$lang['add_biller']                     = "Añadir Sucursal";
+$lang['edit_biller']                    = "Editar Sucursal";
+$lang['delete_biller']                  = "Eliminar Sucursal";
+$lang['delete_billers']                 = "Eliminar Sucursales";
+$lang['biller_added']                   = "Sucursal se  añadio correctamente";
+$lang['biller_updated']                 = "Sucursal se actualizado correctamente";
+$lang['biller_deleted']                 = "Sucursal se ha eliminado correctamente";
+$lang['billers_deleted']                = "Sucursales eliminados correctamente";
+$lang['no_biller_selected']             = "No hay sucursales seleccionado. Por favor seleccione al menos un sucursal.";
+$lang['invoice_footer']                 = "Pie de página de factura";
+$lang['biller_x_deleted_have_sales']    = "Accion fallida, sucursal tiene ventas";
+$lang['billers_x_deleted_have_sales']   = "algunos sucursales no pueden ser eliminados ya que tiene ventas";
